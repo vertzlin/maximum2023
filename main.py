effce6b316be5a4e58c3f2c870a1ae9f126378c5
vertz <arseniykukoba16@gmail.com>
2023-06-26 14:04:11 +0300

    palindr

---
+++ main.py
@@ -0,0 +1,8 @@
+
+def func(c):
+    if c == c[::-1]:
+        return True
+    else:
+        return False
+
+print(func("шалаш"))
---
+++ max.py
@@ -0,0 +1,6 @@
+def func(c):
+    c = input()
+    if c == c[::0]:
+        return True
+    else:
+        return False
